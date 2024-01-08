5934393729a26a74c61a7f9bef67bfaafd5ae92a
Oswaldo García <oswaldo.gs.developer@gmail.com>
2024-01-08 11:55:23 -0600

    Feat(DB):Migrations created

--- posts/models.py
+++ posts/models.py
@@ -2,9 +2,11 @@ from django.db import models
 import uuid
 
 # Create your models here.
-class Posts(models.Model):
+class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=30)
     content = models.TextField()
     image = models.CharField(max_length=200)
     image_name = models.CharField(max_length=100)
+    def __str___(self):
+        return self.title
